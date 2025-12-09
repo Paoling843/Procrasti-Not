17e98eb937e6198fbaced012d2298c1cb439d5c3
Paoling843 <paogeva80@gmail.com>
2025-12-09 16:13:48 +0800

    Added users in sidebar

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -13,7 +13,8 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen }) => {
   const menuItems = [
     { path: "/", label: "Dashboard", icon: <HomeIcon size={24} /> },
     { path: "/settings", label: "Settings", icon: <SettingsIcon size={24} /> },
-    
+    { path: "/users", label: "Users", icon: <SettingsIcon size={24} /> },
+
   ]
 
   if (!isOpen) {
